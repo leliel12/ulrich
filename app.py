f17edf02811c578e618a4a9294ed25535886526e
juanbc <jbcabral@unc.edu.ar>
2022-09-22 20:50:59 -0300

    configuration

--- app.py
+++ app.py
@@ -4,16 +4,16 @@ import pathlib
 import ulrich
 
 
-
 PATH = pathlib.Path(os.path.abspath(os.path.dirname(__file__)))
-CONF_PATH = PATH / "conf.yaml"
+CONF_PATH = PATH / "ulrich_conf.yaml"
 
 if not CONF_PATH.exists():
-    import sys # noqa
-    import yaml # noqa
+    import sys  # noqa
+    import yaml  # noqa
+
     with open(CONF_PATH, "w") as fp:
         yaml.safe_dump(ulrich.DEFAULT_CONF, fp)
-    print(f"New configuration file created at {CONF_PATH!r}")
+    print(f"New configuration file created at '{CONF_PATH!s}'")
     sys.exit(1)
 
 
